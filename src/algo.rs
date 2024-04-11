f239c22c39c07e4c0037b28e129613d08a40f7da
EiEddie <eieddie4u@outlook.com>
2024-04-11 17:34:38 +0800

    feat: 打印路径

--- src/algo.rs
+++ src/algo.rs
@@ -6,7 +6,20 @@ use crate::graph::*;
 #[derive(Debug, Clone)]
 struct Path {
 	/// 储存的是顶点和 "与它上一个顶点间的距离" 组成的 tuple.
-	point: LinkedList<(*const Vert, f64)>,
+	points: LinkedList<(*const Vert, f64)>,
+}
+
+impl std::fmt::Display for Path {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		let mut iter = self.points.iter();
+		write!(f, "[{}]", unsafe { &(*iter.next().unwrap().0).id })?;
+
+		for point in iter {
+			write!(f, " -{}-> [{}]", point.1, unsafe { &(*point.0).id })?;
+		}
+
+		Ok(())
+	}
 }
 
 impl Graph {
@@ -28,12 +41,13 @@ impl Graph {
 		unsafe { (*vert).is_searching = true };
 
 		// 将本顶点放入路径中
-		this_path.point.push_back((vert as *const Vert, dist));
+		this_path.points.push_back((vert as *const Vert, dist));
 
 		// 当顶点已经是终点(之一)时, 保存这条路径
 		// 并继续搜索其余顶点
 		if unsafe { (*vert).is_exit } {
 			paths.push(this_path.clone());
+			this_path.points.pop_back();
 			unsafe { (*vert).is_searching = false };
 			return this_path;
 		}
@@ -46,18 +60,21 @@ impl Graph {
 		// 本顶点的后继的搜索已经完成
 		// 此时本顶点不在路线内, 因为本顶点后面无路可走
 		// 在路线内移除本顶点
-		this_path.point.pop_back();
+		this_path.points.pop_back();
 
 		// 本顶点已被搜索完成, 后续的搜索仍可继续进入本顶点
 		unsafe { (*vert).is_searching = false };
 		return this_path;
 	}
 
+	/// 在指定的起点搜索到所有出口的所有路径.
+	///
+	/// 只要图是联通的, 可以保证至少有一条路, 即 [`Vec`] 内至少有一个元素.
 	#[allow(non_snake_case)]
 	fn DFS(&mut self, start: String) -> Result<Vec<Path>> {
 		let mut paths: Vec<Path> = Vec::new();
 		let start: *mut Vert = self.vert_map.get_mut(&start).ok_or(Error::NoVert)?;
-		let a_path = Path { point: LinkedList::new(), };
+		let a_path = Path { points: LinkedList::new(), };
 
 		self._DFS_(start, 0., a_path, &mut paths);
 
